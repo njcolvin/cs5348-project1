f53a4a4ff655d6a1dbbd29eb3f493b606b442883
njcolvin <njcolvin@live.unc.edu>
2022-09-21 16:08:04 -0500

    refactor and gitignore after removing executable

--- dash.c
+++ dash.c
@@ -3,13 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
-/*
- * This program counts the number of sentences in a given string and
- * the number of tokens each in sentence.
- * Terminates when user enters BYE
- */
 
-size_t buffer_size = 32;
 
 void parse_command(char *buffer, size_t characters)
 {
@@ -19,13 +13,6 @@ void parse_command(char *buffer, size_t characters)
 	char *token_sep = " ";    
     int num_sentences = 0;
 	int num_tokens = 0;
-
-    // The input string consists of several sentences separated by '.'
-    // Each sentence consists of several tokens separated by ' ' (space).
-    // Using strtok_r() find the number of sentences and the number of tokens
-    // in each sentence. HINT: man strtok and look at the sample program at the end.
-    // Print the results.
-    // If the first token is BYE, break from the while loop (check strcmp/strncmp)
 
     // check if user entered EOF (Ctrl+D) or the first token was BYE
     if (characters == -1 || strncmp("exit", buffer, 4) == 0)
@@ -59,9 +46,10 @@ void parse_command(char *buffer, size_t characters)
 int main(int argc, char *argv[])
 {
 
-	
 	char *buffer;
 	size_t characters;
+    size_t buffer_size = 32;
+    FILE *input = stdin;
     
 	// initialize buffer
 	buffer = (char *)malloc(buffer_size * sizeof(char));
@@ -74,38 +62,30 @@ int main(int argc, char *argv[])
         char **pargv = argv+1;
         // check if file can be accessed
         if (access(*pargv, R_OK) == 0) {
-            FILE *fptr;
-            ssize_t read;
-
             // open the file
-            fptr = fopen(*pargv, "r");
-            if (fptr == NULL)
+            input = fopen(*pargv, "r");
+            if (input == NULL)
                 exit(EXIT_FAILURE);
-            
-            // read line by line
-            while ((read = getline(&buffer, &characters, fptr)) != -1) {
-                parse_command(buffer, characters);
-            }
-
-            // close file
-            fclose(fptr);
-
         }
+    }
 
-    } else {
-
-        while (1) {
+    while (1) {
+        if (input == stdin)
             printf("dash> ");
 
-            // use getline() to get the input string
-            characters = getline(&buffer, &buffer_size, stdin);
-
-            // give input from stdin to parse_command
-            parse_command(buffer, characters);
-            
-        }
-
+        // use getline() to get the input string
+        characters = getline(&buffer, &buffer_size, input);
+        
+        // check for EOF or exit command
+        if(characters == -1 || strncmp("exit", buffer, 4) == 0)
+            break;
+        
+        // give input from stdin to parse_command
+        parse_command(buffer, characters);
     }
+
+    if (input != stdin)
+        fclose(input);
 
     if (buffer)
         free(buffer);
